e0973a4661efb83a5509877600a1d6e32f65df37
kokily <kokily@naver.com>
2026-05-15 08:43:11 +0900

    Backend Swagger UI

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiTags('인증 (Auth)')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -16,5 +19,17 @@ export class AuthController {
   @Post('login')
   login(@Body() body: AuthDto) {
     return this.authService.login(body);
+  }
+
+  // JWT AuthGuard가 지키고 있는 테스트용 라우트
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '내 정보 확인(토큰 테스트용)' })
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req: any) {
+    return {
+      message: '인증 성공',
+      user: req.user,
+    }
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,18 @@ async function bootstrap() {
 
   // Cors 방지
   app.enableCors();
+
+  // Swagger setting
+  const config = new DocumentBuilder()
+    .setTitle('D&K Dreams Blog API')
+    .setDescription('블로그 관리를 위한 백엔드 API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
